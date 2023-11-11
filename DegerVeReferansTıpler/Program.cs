2cef3aa70187618006e22d967421fbb355215f89
sevvalkaba0@gmail.com <sevvalkaba0@gmail.com>
2023-11-11 16:17:07 +0300

    Yazılım Geliştirici Yetiştirme Kampı 3. Gün C# Temelleri 2

--- DegerVeReferansTıpler/Program.cs
+++ DegerVeReferansTıpler/Program.cs
@@ -11,7 +11,7 @@
             sayi1 = sayi2;
             sayi2 = 65;
             //sayi1 ?? 30
-
+              
             int[] sayilar1 = new int[] {10,20,30};
             int[] sayilar2 = new int[] { 100, 200, 300 };
             sayilar1 = sayilar2;
