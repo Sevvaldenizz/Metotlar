638bcb2d7d6042f2a13ee438fc7e994c72704938
sevvalkaba0@gmail.com <sevvalkaba0@gmail.com>
2023-11-13 15:31:41 +0300

    Yazılım Geliştirici Yetiştirme Kampı 5. Gün C#

---
+++ Oop2/MusteriManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oop2
+{
+    //İş sınıfları // business classes
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            //if (musteri.)
+            //{
+
+            //}
+         
+        }
+    }
+}
---
+++ Oop2/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Oop2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Sevval";
+            musteri1.Soyadi = "Deniz";
+            musteri1.TcNo = "12345";
+
+                //Kodlama.ip
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "54321";
+            musteri2.SirketAdi = "Kodlama.io";
+            musteri2.VergiNo = "1234567890";
+
+            //gerçek Müşteri - Tüzel Müşteri
+            //SOLİD
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri msuteri4 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            MusteriManager.Ekle(musteri2); 
+
+
+        }
+    }
+}
